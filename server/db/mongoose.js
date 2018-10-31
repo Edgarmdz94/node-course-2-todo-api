7ce6f25946fe59fb79c6d3c116362b2eff508f06
Edgar <edgar@optimen.com.mx>
2018-10-30 18:12:25 -0600

    db path

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // 'mongodb://edgarmendoza:1234abcd@ds245523.mlab.com:45523/my-todo-db'
-mongoose.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true });
+mongoose.connect('mongodb://edgarmendoza:1234abcd@ds245523.mlab.com:45523/my-todo-db' || 'mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true });
 
 module.exports = {
   mongoose
